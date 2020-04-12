1d3aa72ea33298a3a15e238295d76cceb8ee6c22
Saurabh Singh <ss_0002@LAP-0093-RMAC-2.local>
2020-04-12 14:59:50 +0530

    Added auc metrics

--- modelling/keras/keras_custom_metrics.py
+++ modelling/keras/keras_custom_metrics.py
@@ -1,3 +1,15 @@
 def rmse(y_true, y_pred):
     import tensorflow.keras.backend as K
     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+
+def auc(y_true, y_pred):
+    from sklearn import metrics
+    import tensorflow as tf
+
+    def fallback_auc(y_true, y_pred):
+        try:
+            return metrics.roc_auc_score(y_true, y_pred)
+        except:
+            return 0.5
+    return tf.py_function(fallback_auc, (y_true, y_pred), tf.double)
