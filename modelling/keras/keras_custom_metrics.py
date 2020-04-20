a06b7c809257a637939fa5286a32542f285b1612
Saurabh Singh <ss_0002@LAP-0093-RMAC-2.local>
2020-04-20 15:12:49 +0530

    Added custom metrics

--- modelling/keras/keras_custom_metrics.py
+++ modelling/keras/keras_custom_metrics.py
@@ -13,3 +13,35 @@ def auc(y_true, y_pred):
         except:
             return 0.5
     return tf.py_function(fallback_auc, (y_true, y_pred), tf.double)
+
+
+def recall(y_true, y_pred):
+    # from tensorflow.keras import backend as K
+    from keras import backend as K
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    r = true_positives / (possible_positives + K.epsilon())
+    return r
+
+
+def precision(y_true, y_pred):
+    # from tensorflow.keras import backend as K
+    from keras import backend as K
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    p = true_positives / (predicted_positives + K.epsilon())
+    return p
+
+
+def f1(y_true, y_pred):
+    # from tensorflow.keras import backend as K
+    from keras import backend as K
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    return 2 * ((p*r) / (p+r+K.epsilon()))
+
+
+def f1_sklearn(y_true, y_pred):
+    from sklearn.metrics import f1_score
+    return f1_score(y_true, y_pred)
+
